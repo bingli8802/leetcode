0db15376b11b628a2cdc44072149dad584310e94
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-13 08:32:03 -0700

    Update 344_Reverse_String.py

--- 344_Reverse_String.py
+++ 344_Reverse_String.py
@@ -10,9 +10,13 @@ class Solution(object):
         """
         s[:] = s[::-1]
         """
-        # mid = int(len(s)/2)
-        i = 0
-        j = len(s) - 1
-        while i < j:
-            s[i], s[j] = s[j], s[i]
-            i, j = i + 1, j - 1
+        
+        mid = int(len(s)/2)
+        for i in xrange(mid):
+            s[i], s[len(s)-1-i] = s[len(s)-1-i], s[i]
+        
+#         i = 0
+#         j = len(s) - 1
+#         while i < j:
+#             s[i], s[j] = s[j], s[i]
+#             i, j = i + 1, j - 1
