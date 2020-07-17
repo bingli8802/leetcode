09f9e6cc5e9ac9db00ee318eb6387820368390fb
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-16 22:48:27 -0700

    Create 1332_Remove_Palindromic_Subsequences.py

---
+++ 1332_Remove_Palindromic_Subsequences.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removePalindromeSub(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s == "":
+            return 0
+        elif s[:] == s[::-1]:
+            return 1
+        else:
+            return 2
