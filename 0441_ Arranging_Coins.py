6ed01e27fadf7030006abe90f4a5e6134af666a1
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-20 15:00:56 -0700

    Create 0441_ Arranging_Coins.py

---
+++ 0441_ Arranging_Coins.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """      
+        if n == 0:
+            return 0
+        
+        left = 1
+        right = n
+        
+        while left < right:
+            target =  (left + right + 1)/2
+            if target*(target+1)/2 == n:
+                return target
+            elif target*(target+1)/2 > n:
+                right = target - 1
+            elif target*(target+1)/2 < n: 
+                left = target
+        return left
+
