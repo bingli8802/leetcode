21949737ed0af88a76ab0036f23e73f7e570e10d
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-19 17:55:17 -0700

    Create 0278_First_Bad_Version.py

---
+++ 0278_First_Bad_Version.py
@@ -0,0 +1,22 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        left = 0
+        right = n
+        
+        while (left < right):
+            middle = (left+right)/2
+            if isBadVersion(middle) == True:
+                right = middle
+            else:
+                left = middle + 1
+        return left
