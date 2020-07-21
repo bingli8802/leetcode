c855b95351a54c590eb90d2053db3f4baf1fb90c
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-21 01:06:43 -0700

    Update 0441_ Arranging_Coins.py

--- 0441_ Arranging_Coins.py
+++ 0441_ Arranging_Coins.py
@@ -3,31 +3,34 @@ class Solution(object):
         """
         :type n: int
         :rtype: int
-        """      
+        """  
+#       返回第一个小于x*(x+1)/2<=n 的x
         if n == 0:
             return 0
         
-        left = 0
-        # left = 1
+        # left = 0
+        left = 1
         right = n
         
-        # while left < right:
+        while left < right:
+            target =  (left + right + 1)/2
+            if target*(target+1)/2 == n:
+                return target
+#           严格大于n 则x一定不是 区间就是[left,target-1]
+            elif target*(target+1)/2 > n:
+                right = target - 1
+#           小于n 则x有可能就是 区间应该是[target, right]
+            elif target*(target+1)/2 < n: 
+                left = target
+        return left
+    
+        # while left <= right:
         #     target =  (left + right + 1)/2
         #     if target*(target+1)/2 == n:
         #         return target
         #     elif target*(target+1)/2 > n:
         #         right = target - 1
         #     elif target*(target+1)/2 < n: 
-        #         left = target
-        # return left
-    
-        while left <= right:
-            target =  (left + right + 1)/2
-            if target*(target+1)/2 == n:
-                return target
-            elif target*(target+1)/2 > n:
-                right = target - 1
-            elif target*(target+1)/2 < n: 
-                left = target + 1
-        return right
+        #         left = target + 1
+        # return right
 
