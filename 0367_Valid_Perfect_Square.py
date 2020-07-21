dfec67c4ea4dc5763e1bcb8081725e668a39e4a2
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-21 11:56:53 -0700

    Update 0367_Valid_Perfect_Square.py

--- 0367_Valid_Perfect_Square.py
+++ 0367_Valid_Perfect_Square.py
@@ -48,3 +48,28 @@ class Solution(object):
             elif mid * mid < num:
                     left = mid + 1
         return False
+
+# 第三次看完教程
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+#       退出循环条件：
+#       left+1大于right 或者right-1小于left 循环结束
+        
+        left = 0
+        right = num
+        
+        while left <= right:
+            mid = (left+right)/2
+            if mid * mid == num:
+                return True
+            # 严格大于 target 的元素一定不是解
+            elif mid * mid > num:
+                    right = mid - 1
+            # 严格小于 target 的元素一定不是解
+            elif mid * mid < num:
+                    left = mid + 1
+        return False
