efe96291e2ed109ec3f93ef0debf6c1a7bfdaab7
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-21 09:34:04 -0700

    Update 0704_Binary_Search.py

--- 0704_Binary_Search.py
+++ 0704_Binary_Search.py
@@ -4,13 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :type target: int
         :rtype: int
-        """
+        """        
+#         left = 0
+#         right = len(nums)-1
         
+#         while (left <= right):
+#             middle = (left + right)/2
+#             if nums[middle] == target:
+#                 return middle
+#             elif nums[middle] < target:
+#                 left = middle + 1
+#             elif nums[middle] > target:
+#                 right = right - 1
+#         return -1
+
         left = 0
-        right = len(nums)-1
+        right = len(nums)
         
-        while (left <= right):
-            middle = (left + right)/2
+        while (left < right):
+            middle = (left+right)/2
             if nums[middle] == target:
                 return middle
             elif nums[middle] < target:
