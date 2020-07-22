1d2861a046b3bc2df48c996a5edf1fbdcf8d62c8
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-21 20:43:26 -0700

    Update 0035_Search_Insert_Position.py

--- 0035_Search_Insert_Position.py
+++ 0035_Search_Insert_Position.py
@@ -23,4 +23,27 @@ class Solution(object):
             elif nums[middle] > target:
                 # 下一轮搜索区间是 [left, mid]
                 right = middle 
-        return right
+        return left
+    
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        left = 0
+        right = len(nums)-1
+        
+
+        while left <= right:
+            mid = (left + right)/2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+        return left
+        
