ade3bddb6707d27c533de0d891a127fbedda2039
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-22 19:58:07 -0700

    Create 0074_Search_a_2D_Matrix.py

---
+++ 0074_Search_a_2D_Matrix.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if matrix == []:
+            return False
+        
+        row = len(matrix)
+        col = len(matrix[0])
+        
+        left = 0
+        right = row * col - 1
+        
+        while left <= right:
+            middle = (left+right)/2
+            r = middle/col
+            c = middle%col
+            if matrix[r][c] == target:
+                return True
+            elif matrix[r][c] < target:
+                left = middle + 1
+            elif matrix[r][c] > target:
+                right = middle - 1
+        return False
