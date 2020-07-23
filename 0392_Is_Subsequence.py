b9950d84080f1e59573fd93c77493ae19b715744
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-22 23:47:05 -0700

    Update 0392_Is_Subsequence.py

--- 0392_Is_Subsequence.py
+++ 0392_Is_Subsequence.py
@@ -6,11 +6,14 @@ class Solution(object):
         :rtype: bool
         """
         i = 0
+        j = 0
         
-        while i < len(s) and i < len(t):
-            if s[i] in t[i:]:
-                i += 1
+        while i<len(s) and j<len(t):
+            if s[i] == t[j]:
+                i+=1
+                j+=1
             else:
-                return False
-        return True
+                j+=1
+                
+        return i==len(s)
             
