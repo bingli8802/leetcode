3829ecb00ef51ab9df126e1f4490bbaa5209c958
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-25 00:35:11 -0700

    Update 0035_Search_Insert_Position.py

--- 0035_Search_Insert_Position.py
+++ 0035_Search_Insert_Position.py
@@ -1,3 +1,4 @@
+# 第一次复习答案
 class Solution(object):
     def searchInsert(self, nums, target):
         """
@@ -7,13 +8,11 @@ class Solution(object):
         """
 #       寻找第一个大于目标值的位置 最后一个元素也可能是 所以right=len(nums)
 #       循环退出的条件是当left==right 所以返回left或者right都可以
-
         left = 0
         right = len(nums)
         
         while left < right:
             middle = (left+right)/2
-            
             if nums[middle] == target:
                 return middle
             # 严格小于 target 的元素一定不是解
@@ -25,25 +24,23 @@ class Solution(object):
                 right = middle 
         return left
     
-class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+# class Solution(object):
+#     def searchInsert(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: int
+#         """
 
-        left = 0
-        right = len(nums)-1
-        
-
-        while left <= right:
-            mid = (left + right)/2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                left = mid + 1
-            elif nums[mid] > target:
-                right = mid - 1
-        return left
+#         left = 0
+#         right = len(nums)-1
+#         while left <= right:
+#             mid = (left + right)/2
+#             if nums[mid] == target:
+#                 return mid
+#             elif nums[mid] < target:
+#                 left = mid + 1
+#             elif nums[mid] > target:
+#                 right = mid - 1
+#         return left
         
