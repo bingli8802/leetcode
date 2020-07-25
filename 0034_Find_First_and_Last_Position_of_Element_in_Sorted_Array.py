ef611c4915d5a15f1e3fab25a8bb25436ceaf178
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-25 15:03:09 -0700

    Update 0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py

--- 0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ 0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -40,3 +40,45 @@ class Solution(object):
         return [start,end]
         
             
+# 第一次复习写的答案
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums) == 0:
+            return[-1,-1]
+        # 第一次二分查找，找最左边target
+        left = 0
+        right = len(nums)-1
+        ret = []
+        while left < right:
+            mid = (left+right)/2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        # 如果没找到直接返回，如果找到的话继续第二次二分查找，找最右边的target
+        if nums[right] != target:
+            return [-1,-1]
+        else:
+            # 这次的查找范围应该是在[left,right]之间，而不是[0,right],可以缩小范围
+            ret.append(left)
+            right = len(nums)-1
+            while left < right:
+                mid = (left+right+1)/2
+                if nums[mid] < target:
+                    left = mid + 1
+                elif nums[mid] > target:
+                    right = mid - 1
+                elif nums[mid] == target:
+                        left = mid
+            ret.append(right)
+        return ret
+                
+              
+            
+            
+            
