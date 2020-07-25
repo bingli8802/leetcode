0d704002137ae2f48d16762d29f9a62a99a50fe1
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-25 11:03:02 -0700

    Update 0209_Minimum_Size_Subarray_Sum.py

--- 0209_Minimum_Size_Subarray_Sum.py
+++ 0209_Minimum_Size_Subarray_Sum.py
@@ -5,17 +5,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+#       start，end 两个指针右移
+#       如果start：end之和小于target，end右移
+#       如果start：end之和大于target，start右移
+#       每次比较ret的长度
         start = 0
         end = 0
         total = 0
-        ret = len(nums) + 1
+        ret = len(nums)+1
         
         while end < len(nums):
-            total = total + nums[end]
+            total += nums[end]
             while total >= s:
-                total = total - nums[start]
-                ret = min(ret,end-start+1)
-                start = start + 1
-            end = end + 1
-        return 0 if ret == len(nums) + 1 else ret
+                ret = min(ret, end-start+1)
+                total -= nums[start]
+                start += 1
+            end += 1
+        return 0 if ret == len(nums)+1 else ret
