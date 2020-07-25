be912d257f065f8e14e253d3a4f82c5c733e59af
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-25 00:23:55 -0700

    Update 0374_Guess_Number_Higher_or_Lower.py

--- 0374_Guess_Number_Higher_or_Lower.py
+++ 0374_Guess_Number_Higher_or_Lower.py
@@ -11,15 +11,13 @@ class Solution(object):
         """
         left = 1
         right = n
-        
-        
-#         while(left <= right):也行 最终【left，right】一定是左边小于右边
+#       用 (left <= right)也行 最终[left，right] 一定是左边小于右边
         while(left<right):
             middle = (left+right)/2
             if guess(middle) == 0:
                 return middle
             elif guess(middle) == 1:
                 left = middle + 1
-            elif guess(middle) == -1:
+            else:
                 right = middle - 1
         return left
