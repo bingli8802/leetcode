c46516d66ea700520f71dde1359d8eeddef42289
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-24 22:52:53 -0700

    Update 0852_Peak_Index_in_a_Mountain_Array.py

--- 0852_Peak_Index_in_a_Mountain_Array.py
+++ 0852_Peak_Index_in_a_Mountain_Array.py
@@ -4,13 +4,14 @@ class Solution(object):
         :type A: List[int]
         :rtype: int
         """
+#       对于本题，使用right = len(A)-1 或者right = len(A)都通过
+#       因为要跟紧邻的右边的数字比较，这样初始化 right 的意义在于 mid+1 就不会越界了
         left = 0
-        right = len(A) - 1
-        
+        right = len(A)-1
         while left < right:
             mid = (left+right)/2
-            if A[mid] < A[mid+1]:
-                left = mid + 1
-            elif A[mid] > A[mid+1]:
+            if A[mid]>A[mid+1]:
                 right = mid
-        return left
+            else:
+                left = mid + 1
+        return right
