9e493c348cf879fa6b419ff1af0f03f489c075f9
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-25 00:53:20 -0700

    Update 0441_ Arranging_Coins.py

--- 0441_ Arranging_Coins.py
+++ 0441_ Arranging_Coins.py
@@ -4,27 +4,21 @@ class Solution(object):
         :type n: int
         :rtype: int
         """  
-#       返回第一个小于x*(x+1)/2<=n 的x
-        # if n == 0:
-        #     return 0
-        
+#       返回 x*(x+1)/2 <= n 最大x的值
         left = 0
-        # left = 1
         right = n
-        
         while left < right:
-#         如果选择左边界不能排除中位数，则中位数应该取右中位数            
-            target =  (left + right + 1)/2
-    
-            if target*(target+1)/2 == n:
-                return target
-#           严格大于n 则x一定不是 区间就是[left,target-1]
-            elif target*(target+1)/2 > n:
-                right = target - 1
-#           小于n 则x有可能就是 区间应该是[target, right]
-            elif target*(target+1)/2 < n: 
+#           如果选择左边界不能排除中位数，则中位数应该取右中位数            
+            mid =  (left + right + 1)/2
+            if mid*(mid+1)/2 == n:
+                return mid
+#           严格大于n 则x一定不是 区间就是[left,mid-1]
+            elif mid*(mid+1)/2 > n:
+                right = mid - 1
+#           小于n,则mid有可能就是目标值，区间应该是[mid, right]
+            else: 
 #         如果选择左边界不能排除中位数，则中位数应该取右中位数
-                left = target 
+                left = mid 
         return left
     
         # while left <= right:
