1b03205e7e01b493a90bda15b491c937bc1ad0b2
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-25 00:18:06 -0700

    Update 0744_Find_Smallest_Letter_Greater_Than_Target.py

--- 0744_Find_Smallest_Letter_Greater_Than_Target.py
+++ 0744_Find_Smallest_Letter_Greater_Than_Target.py
@@ -16,16 +16,34 @@
 #                 left = middle + 1
 #         return letters[left] if letters[left] > target else letters[0]
 
+# class Solution(object):
+#     def nextGreatestLetter(self, letters, target):
+#         left = 0 
+#         right = len(letters) - 1
+#         n =  len(letters)
+#         while left <= right:
+#             mid = left + (left - right) / 2
+#             if target >= letters[mid]:
+#                 left = mid + 1
+#             else:
+#                 right = mid - 1
+            
+#         return letters[left % n]
+
+# 第一遍复习写出
 class Solution(object):
     def nextGreatestLetter(self, letters, target):
-        start = 0 
-        end = len(letters) - 1
-        n =  len(letters)
-        while start <= end:
-            mid = start + (end - start) / 2
-            if target >= letters[mid]:
-                start = mid + 1
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        left = 0
+        right = len(letters)
+        while left < right:
+            mid = (left+right)/2
+            if letters[mid] <= target:
+                left = mid + 1
             else:
-                end = mid - 1
-            
-        return letters[start % n]
+                right = mid
+        return letters[right%len(letters)]
