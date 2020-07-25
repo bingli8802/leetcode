19eed6396bcb74c8696db6999d4d7ad779d04066
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-25 15:26:33 -0700

    Update 0275_H-Index_II.py

--- 0275_H-Index_II.py
+++ 0275_H-Index_II.py
@@ -6,14 +6,15 @@ class Solution(object):
         """
         left = 0
         right = len(citations)-1
-        
-        while left<=right:
+        while left <= right:
             mid = (left+right)/2
+            # 判断中间值很巧妙
             if citations[mid] == len(citations)-mid:
                 return citations[mid]
             elif citations[mid] > len(citations)-mid:
-                right = mid-1
+                right = mid - 1
             else:
-                left = mid+1
-        return len(citations)-left
+                left = mid + 1
+        # 返回值也很巧妙运用减法，这样如果citations==[]也可以返回0
+        return len(citations) - left
 
