c0d8a3744fec55d9dc5c60d1a5ff7a9183a3c903
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-25 10:29:09 -0700

    Update 0240_Search_a_2D_Matrix_II.py

--- 0240_Search_a_2D_Matrix_II.py
+++ 0240_Search_a_2D_Matrix_II.py
@@ -1,3 +1,29 @@
+# class Solution(object):
+#     def searchMatrix(self, matrix, target):
+#         """
+#         :type matrix: List[List[int]]
+#         :type target: int
+#         :rtype: bool
+#         """
+#         if len(matrix) == 0 or len(matrix[0]) == 0:
+#             return False
+        
+#       考虑左下角或者右上角的两个点
+#       左下角
+#         x = len(matrix)-1
+#         y = 0
+#         while True:
+#             if matrix[x][y] == target:
+#                 return True
+#             elif matrix[x][y] < target:
+#                 y = y + 1
+#             elif matrix[x][y] > target:
+#                 x = x - 1
+            
+#             if x < 0 or y >= len(matrix[0]):
+#                 return False
+        
+# 第一遍复习写出答案
 class Solution(object):
     def searchMatrix(self, matrix, target):
         """
@@ -8,19 +34,17 @@ class Solution(object):
         if len(matrix) == 0 or len(matrix[0]) == 0:
             return False
         
-#       考虑左下角或者右上角的两个点
-#       左下角
-        x = len(matrix)-1
-        y = 0
-        while True:
-            if matrix[x][y] == target:
+        m = len(matrix)
+        n = len(matrix[0])
+        i = m-1
+        j = 0
+        while i >= 0 and j < n:
+            if matrix[i][j] == target:
                 return True
-            elif matrix[x][y] < target:
-                y = y + 1
-            elif matrix[x][y] > target:
-                x = x - 1
-            
-            if x < 0 or y >= len(matrix[0]):
-                return False
-        
+            elif matrix[i][j] < target:
+                j += 1
+            else:
+                i -= 1
+        return False
+                
                 
