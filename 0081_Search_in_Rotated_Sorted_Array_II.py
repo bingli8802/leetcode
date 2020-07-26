6723687bbcb7a650cdb918d1b95b9286ab2cad5c
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-25 17:51:13 -0700

    Update 0081_Search_in_Rotated_Sorted_Array_II.py

--- 0081_Search_in_Rotated_Sorted_Array_II.py
+++ 0081_Search_in_Rotated_Sorted_Array_II.py
@@ -3,10 +3,10 @@ class Solution(object):
         """
         :type nums: List[int]
         :type target: int
-        :rtype: bool
+        :rtype: int
         """
         left = 0
-        right = len(nums) - 1
+        right = len(nums)-1
         while left <= right:
             mid = (left+right)/2
             if nums[mid] == target:
@@ -15,17 +15,54 @@ class Solution(object):
 #           如果相等，左边界右移，直到不相等
             if nums[left] == nums[right]:
                 left += 1
-                continue
-
-            if nums[left] <= nums[mid]:
-                if nums[left] <= target and target < nums[mid]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            else:
+                # right -= 1
+                continue          
+            # 如果中间数小于最左边的数，那么[mid,right]这一区间是单调升的
+            if nums[mid] < nums[left]:
+                # 如果target在单调升这一区间
                 if nums[mid] < target and target <= nums[right]:
                     left = mid + 1
                 else:
                     right = mid - 1
-        
+            # 如果中间数大于最左边的数，那么[left,mid]这一区间是单调升的
+            elif nums[mid] >= nums[left]:
+                if nums[left] <= target and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return False
+    
+#  第二种解法
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            mid = (left+right)/2
+            if nums[mid] == target:
+                return True
+#           判断左边界和右边界是否相等
+#           如果相等，左边界右移，直到不相等
+            if nums[left] == nums[right]:
+                left += 1
+                # right -= 1
+                continue          
+            # 如果中间数小于或等于最右边的数，那么[mid,right]这一区间是单调升的
+            if nums[mid] <= nums[right]:
+                # 如果target在单调升这一区间
+                if nums[mid] < target and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            # 如果中间数大于最右边的数，那么[left,mid]这一区间是单调升的
+            elif nums[mid] > nums[right]:
+                if nums[left] <= target and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
         return False
