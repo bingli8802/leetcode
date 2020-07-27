d4d31ef81f21f448d8891b8d519e4256c823bf37
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-27 14:50:29 -0700

    Update 0682_Baseball_Game.py

--- 0682_Baseball_Game.py
+++ 0682_Baseball_Game.py
@@ -13,12 +13,20 @@ class Solution(object):
             elif op == "C" and li:
                 li.pop()
             elif op == "+":
-                if len(li) == 1:
-                    sum = li[-1]
-                elif len(li) >= 2:
-                    sum = li[-1] + li[-2]
-                li.append(sum)
+                # if len(li) == 1:
+                #     sum = li[-1]
+                # elif len(li) >= 2:
+                #     sum = li[-1] + li[-2]
+                # li.append(sum)
+                
+                # 或者直接一句话
                 # li.append(li[-1] + li[-2])
+                
+                # 或者完全用栈的操作
+                top = li.pop()
+                new_top = top + li[-1]
+                li.append(top)
+                li.append(new_top)
             else:
                 li.append(int(op))
         for i in li:
