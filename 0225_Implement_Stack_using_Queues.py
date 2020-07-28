b8c3ebe80c904eb1edfccd2e93502631fa1484ef
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-27 17:34:25 -0700

    Update 0225_Implement_Stack_using_Queues.py

--- 0225_Implement_Stack_using_Queues.py
+++ 0225_Implement_Stack_using_Queues.py
@@ -51,6 +51,70 @@ class MyStack(object):
         """
         return True if len(self.queue1) == 0 else False
 
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
+
+# 第二种做法
+# 定义一个全局变量 stack_top
+class MyStack(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue1 = []
+        self.queue2 = []
+        self.stack_top = ""
+        
+    def push(self, x):
+        """
+        Push element x onto stack.
+        :type x: int
+        :rtype: None
+        """
+        self.queue1.append(x)
+        self.stack_top = x
+
+    def pop(self):
+        """
+        Removes the element on top of the stack and returns that element.
+        :rtype: int
+        """
+        while len(self.queue1) > 1:
+            self.stack_top = self.queue1.pop(0)
+            self.queue2.append(self.stack_top)
+        top = self.queue1.pop(0)
+        tem = self.queue1 
+        self.queue1 = self.queue2
+        self.queue2 = tem        
+        return top
+            
+    def top(self):
+        """
+        Get the top element.
+        :rtype: int
+        """
+        # while len(self.queue1) > 1:
+        #     top = self.queue1.pop(0)
+        #     self.queue2.append(top)
+        # print "ok"    
+        # top = self.queue1.pop(0)
+        # self.queue2.append(top)
+        # tem = self.queue1 
+        # self.queue1 = self.queue2
+        # self.queue2 = tem
+        return self.stack_top
+
+    def empty(self):
+        """
+        Returns whether the stack is empty.
+        :rtype: bool
+        """
+        return True if len(self.queue1) == 0 else False
 
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()
