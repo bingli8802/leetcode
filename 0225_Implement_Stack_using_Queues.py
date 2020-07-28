86ee42348873073d0815d2a6ad042c90092f5d33
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-27 17:25:16 -0700

    Create 0225_Implement_Stack_using_Queues.py

---
+++ 0225_Implement_Stack_using_Queues.py
@@ -0,0 +1,60 @@
+class MyStack(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue1 = []
+        self.queue2 = []
+
+    def push(self, x):
+        """
+        Push element x onto stack.
+        :type x: int
+        :rtype: None
+        """
+        self.queue1.append(x)
+
+    def pop(self):
+        """
+        Removes the element on top of the stack and returns that element.
+        :rtype: int
+        """
+        while len(self.queue1) > 1:
+            self.queue2.append(self.queue1.pop(0))
+        top = self.queue1.pop(0)
+        tem = self.queue1 
+        self.queue1 = self.queue2
+        self.queue2 = tem        
+        return top
+            
+    def top(self):
+        """
+        Get the top element.
+        :rtype: int
+        """
+        while len(self.queue1) > 1:
+            top = self.queue1.pop(0)
+            self.queue2.append(top)
+        print "ok"    
+        top = self.queue1.pop(0)
+        self.queue2.append(top)
+        tem = self.queue1 
+        self.queue1 = self.queue2
+        self.queue2 = tem
+        return top
+
+    def empty(self):
+        """
+        Returns whether the stack is empty.
+        :rtype: bool
+        """
+        return True if len(self.queue1) == 0 else False
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
