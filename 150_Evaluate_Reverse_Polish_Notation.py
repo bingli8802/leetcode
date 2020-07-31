6f3d0ca3fa9e826c2254130d7b6b325254dcc862
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-07-30 23:13:36 -0700

    Create 150_Evaluate_Reverse_Polish_Notation.py

---
+++ 150_Evaluate_Reverse_Polish_Notation.py
@@ -0,0 +1,31 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        operators = ["+", "-", "*", "/"]
+        stack = []
+        res = 0
+        for i in tokens:
+            # 判断负数
+            if i.lstrip("-").isdigit():
+                stack.append(i)
+            elif i in operators:
+                top1 = stack.pop()
+                top2 = stack.pop()
+                res = str(int(eval(top2 + i + top1)))
+                stack.append(res)
+        return stack[0]
+        
+解法2 不去判断数字       
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        operators = ["+", "-", "*", "/"]
+        stack = []
+        res = 0
+        for i in tokens:
+            if i in operators:
+                top1 = stack.pop()
+                top2 = stack.pop()
+                res = str(int(eval(top2 + i + top1)))
+                stack.append(res)
+            else:
+                stack.append(i)
+        return stack[0]
