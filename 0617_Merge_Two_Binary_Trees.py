7a3fe1943916b790c98a577e847bf7205479f779
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-08-05 22:44:11 -0700

    Create 0617_Merge_Two_Binary_Trees.py

---
+++ 0617_Merge_Two_Binary_Trees.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        # 直接在t1上面操作 更改
+        if t1 == None:
+            return t2
+        elif t2 == None:
+            return t1
+        t1.val = t1.val + t2.val
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+        return t1
