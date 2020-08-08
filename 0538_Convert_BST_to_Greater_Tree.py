35449e71e67710a5a45fd55616cf2975201d2605
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-08-07 17:13:14 -0700

    Create 0538_Convert_BST_to_Greater_Tree.py

---
+++ 0538_Convert_BST_to_Greater_Tree.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    # 定义一个全局变量
+    def __init__(self):
+        self.total = 0
+        
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root == None:
+            return
+        # 先遍历右子树 更新当前总和 并赋值给root
+        self.convertBST(root.right)
+        self.total += root.val
+        root.val = self.total
+        # 再遍历左子树
+        self.convertBST(root.left)
+        return root
+
+# class Solution(object):        
+#     def convertBST(self, root):
+#         """
+#         :type root: TreeNode
+#         :rtype: TreeNode
+#         """
+#         total = 0
+#         return self.searchBST(root, total)
+    
+#     def searchBST(self, root, total):
+#         if root == None:
+#             return
+#         self.searchBST(root.right, total)
+#         total += root.val
+#         root.val = total
+#         self.searchBST(root.left, total)
+#         return root
+
+
