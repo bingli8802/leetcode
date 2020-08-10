8340cc3f104e6490be0c954dae170da66c800941
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-08-10 15:29:25 -0700

    Create 0112_Path_Sum.py

---
+++ 0112_Path_Sum.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# 第一种解法 
+# 每走一个节点 就用sum减去当前节点的值 如果遇到叶子节点 就判断该叶子节点的值和sum是否一样
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if root == None:
+            return False
+        if root.left == None and root.right == None:
+            return root.val == sum
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
+
