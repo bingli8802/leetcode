381688d8a92b8d2ad9678762a9a835ce5e8677c7
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-08-11 16:24:28 -0700

    Update 1315_Sum_of_Nodes_with_Even-Valued_Grandparent.py

--- 1315_Sum_of_Nodes_with_Even-Valued_Grandparent.py
+++ 1315_Sum_of_Nodes_with_Even-Valued_Grandparent.py
@@ -4,6 +4,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# BFS自己做的
 class Solution(object):
     def sumEvenGrandparent(self, root):
         """
@@ -29,4 +31,40 @@ class Solution(object):
                 if root.val % 2 == 0 and root.right.right:
                     total += root.right.right.val
         return total
+# DFS
+class Solution(object):
+    def sumEvenGrandparent(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        
+        self.ans = 0
+        self.dfs(None, None, root)
+        return self.ans
+    
+    def dfs(self, grandparent, parent, node):
+        if node == None:
+            return
+        if grandparent != None and grandparent.val % 2 == 0:
+            self.ans += node.val
+        self.dfs(parent, node, node.left)
+        self.dfs(parent, node, node.right)
+# DFS
+class Solution:
+    def sumEvenGrandparent(self, root):
+        self.ans = 0
+        self.dfs(1, 1, root)
+        return self.ans
+    def dfs(self, gp_val, p_val, node):
+        if not node:
+            return
+        if gp_val % 2 == 0:
+            # nonlocal ans
+            self.ans += node.val
+        self.dfs(p_val, node.val, node.left)
+        self.dfs(p_val, node.val, node.right)
+        
+
+
             
