55fb2ce77adcf0a0e41431b1b4415863fcd3f012
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-08-13 10:51:30 -0700

    Create 0814_Binary_Tree_Pruning.py

---
+++ 0814_Binary_Tree_Pruning.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def pruneTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root == None:
+            return None
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        if root.val == 0 and root.left == None and root.right == None:
+            root = None
+        return root
