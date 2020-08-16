62a7d81a28c6bf48e3eb8953aa19f0d347277d79
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-08-16 12:17:56 -0700

    Update 0889_Construct_Binary_Tree_from_Preorder_and_Postorder_Traversal.py

--- 0889_Construct_Binary_Tree_from_Preorder_and_Postorder_Traversal.py
+++ 0889_Construct_Binary_Tree_from_Preorder_and_Postorder_Traversal.py
@@ -17,6 +17,29 @@ class Solution(object):
         :type post: List[int]
         :rtype: TreeNode
         """
+        if not pre:
+            return None
+        # 数组长度为1时，直接返回即可
+        if len(pre) == 1:
+            return TreeNode(pre[0])
+        # 根据前序数组的第一个元素，创建根节点     
+        root = TreeNode(pre[0])
+        # 根据前序数组第二个元素，确定后序数组左子树范围
+        left_count = post.index(pre[1]) + 1
+        # 递归执行前序数组左边、后序数组左边
+        root.left = self.constructFromPrePost(pre[1:left_count + 1],post[:left_count])
+        # 递归执行前序数组右边、后序数组右边
+        root.right = self.constructFromPrePost(pre[left_count + 1:],post[left_count:-1])
+        # 返回根节点
+        return root
+
+class Solution(object):
+    def constructFromPrePost(self, pre, post):
+        """
+        :type pre: List[int]
+        :type post: List[int]
+        :rtype: TreeNode
+        """
         def dfs(pre, post):
             if not pre:
                 return None
