09401c13ba77a0c4afab6d0097e8615ac9973d4e
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-08-17 08:45:43 -0700

    Update 0590_N-ary_Tree_Postorder_Traversal.py

--- 0590_N-ary_Tree_Postorder_Traversal.py
+++ 0590_N-ary_Tree_Postorder_Traversal.py
@@ -15,6 +15,7 @@ class Solution(object):
         if root is None:
             return []
         res = []
+        # 先遍历每一个子节点 再把root入栈
         for child in root.children:
             res.extend(self.postorder(child))
         res.append(root.val)
@@ -29,6 +30,7 @@ class Solution(object):
         def post(root):
             if not root: 
                 return []
+            # 先遍历每一个子节点 再把root入栈
             for node in root.children:
                 post(node)
             res.append(root.val)
@@ -48,8 +50,11 @@ class Solution(object):
         stack = [root]
         while stack:
             cur = stack.pop()
+            # 根节点入栈
             res.append(cur.val)
+            # 从最左边孩子入栈
             for node in cur.children:
                 stack.append(node)
+        # 结果要反转
         return res[::-1]
 
