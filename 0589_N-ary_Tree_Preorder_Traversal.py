b3690973bd82b7ced699535b50913b684f725fb2
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-08-16 23:43:20 -0700

    Create 0589_N-ary_Tree_Preorder_Traversal.py

---
+++ 0589_N-ary_Tree_Preorder_Traversal.py
@@ -0,0 +1,53 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+# 递归1
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if not root: 
+            return []
+        res = [root.val]
+        for node in root.children:
+            res.extend(self.preorder(node))
+        return res
+# 递归2
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        def pre(root):  
+            if not root:
+                return
+            res.append(root.val)
+            for child in root.children:
+                pre(child)
+        res = []
+        pre(root)
+        return res
+# 迭代
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if not root:
+            return None
+        res = []
+        stack = [root]
+        while stack:
+            cur = stack.pop()
+            res.append(cur.val)
+            for child in cur.children[::-1]:
+                stack.append(child)
+        return res
