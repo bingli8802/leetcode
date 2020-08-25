e6094684dbee9b632df27f6c3b7f16d4da34d9be
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-08-24 22:11:42 -0700

    Create 1512_Number_of_Good_Pairs.py

---
+++ 1512_Number_of_Good_Pairs.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = 0
+        for i in set(nums):
+            count_i = nums.count(i)
+            ans += count_i * (count_i - 1) / 2
+        return ans
+    
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """        
+        ans = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    ans += 1
+        return ans
+    
+    # 最快
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """ 
+        m = collections.Counter(nums)
+        return sum(v * (v - 1) // 2 for k, v in m.items())
+
+
