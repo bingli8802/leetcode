c35bfc8437b48e69d14a256f6899139667b2b815
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-08-26 23:04:25 -0700

    Create 1450_Number_of_Students_Doing_Homework_at_a_Given_Time.py

---
+++ 1450_Number_of_Students_Doing_Homework_at_a_Given_Time.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def busyStudent(self, startTime, endTime, queryTime):
+        """
+        :type startTime: List[int]
+        :type endTime: List[int]
+        :type queryTime: int
+        :rtype: int
+        """
+        res = 0
+        for i in range(len(startTime)):
+            if startTime[i] <= queryTime <= endTime[i]:
+                res += 1
+        return res
+    
+    def busyStudent(self, startTime, endTime, queryTime):
+        """
+        :type startTime: List[int]
+        :type endTime: List[int]
+        :type queryTime: int
+        :rtype: int
+        """
+        return sum([i <= queryTime <=j for i, j in zip(startTime, endTime)])
