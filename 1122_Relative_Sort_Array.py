bc0590cf095bf9249c583ebc3d7d21c8ee18264f
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-09-06 16:19:54 -0700

    Create 1122_Relative_Sort_Array.py

---
+++ 1122_Relative_Sort_Array.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+        """
+        :type arr1: List[int]
+        :type arr2: List[int]
+        :rtype: List[int]
+        """
+        from collections import Counter
+        c = Counter(arr1)
+        hashmap = dict(c)
+        res = []
+        for i in arr2:
+            for j in range(hashmap.get(i)):
+                res.append(i)
+        diff = [item for item in arr1 if item not in arr2]
+        return res + sorted(diff)
+    
+    def relativeSortArray(self, arr1, arr2):
+        """
+        :type arr1: List[int]
+        :type arr2: List[int]
+        :rtype: List[int]
+        """
+        from collections import defaultdict
+        dic = defaultdict(int)
+        for k, v in enumerate(arr2):
+            dic[v] = k
+        li = [[] for i in range(len(arr2) + 1)]
+        for num in arr1:
+            index = dic.get(num)
+            if index is not None:
+                li[index].append(num)
+            else:
+                li[-1].append(num)
+        li[-1].sort()
+        return [x for sublist in li for x in sublist]
