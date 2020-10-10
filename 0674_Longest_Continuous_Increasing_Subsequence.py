84a3e087f632d43bc26a1aea8b7a221df79cb3a8
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-10-09 23:45:18 -0700

    Update 0674_Longest_Continuous_Increasing_Subsequence.py

--- 0674_Longest_Continuous_Increasing_Subsequence.py
+++ 0674_Longest_Continuous_Increasing_Subsequence.py
@@ -34,3 +34,24 @@ class Solution(object):
                 cnt = 1
             ans = max(ans, cnt)
         return ans
+    
+    # 效率最高 接近100%
+    def findLengthOfLCIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 0:
+            return 0
+        # if n == 1:
+        #     return 1
+        res = 1
+        cnt = 1
+        for i in range(1, n):
+            if nums[i] > nums[i-1]:
+                cnt += 1
+            else:
+                res = max(res, cnt)
+                cnt = 1
+        return max(res, cnt)
