9114950f0f184b44030d5e1ed800f08f0a2328fc
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-12-04 14:37:18 -0800

    Update 0053_Maximum_Subarray.py

--- 0053_Maximum_Subarray.py
+++ 0053_Maximum_Subarray.py
@@ -13,3 +13,33 @@ class Solution(object):
             res = max(res, curSum)
         return res
     
+    # 举一反三 返回左右边界
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        res = nums[0]
+        curSum = 0
+        leftBound = 0
+        rightBound = 0
+        tmpBound = 0
+        
+        for i in range(n):
+            if curSum < 0:
+                tmpBound = i
+                curSum = nums[i]
+            else:
+                curSum += nums[i]
+            
+            if curSum > res:
+                res = curSum
+                leftBound = tmpBound
+                rightBound = i
+                
+        print leftBound, rightBound
+        tmp = sum(nums[leftBound:rightBound+1])
+        # tmp 和 res 相等
+        return res
+            
