c06a1eb618f361f884fe9d8661d38787fe067d27
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-12-07 22:20:24 -0800

    Create 0064_Minimum_Path_Sum.py

---
+++ 0064_Minimum_Path_Sum.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        if not grid or not grid[0]:
+            return 0
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[i][j] = grid[0][0]
+                    continue
+                elif i == 0:
+                    dp[0][j] = dp[0][j-1] + grid[0][j]
+                elif j == 0:
+                    dp[i][0] = dp[i-1][0] + grid[i][0]
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+        return dp[-1][-1]
+                
