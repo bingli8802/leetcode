2827f3cb1f5e848bf4c224ad182a41402a8a19e9
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-12-09 15:47:44 -0800

    Update 0263_Ugly_Number.py

--- 0263_Ugly_Number.py
+++ 0263_Ugly_Number.py
@@ -10,10 +10,11 @@ class Solution(object):
         nums = [2,3,5]
         
         for i in nums:
-            # 如果这个数能被2，3，5 整除 就继续整除
-            # 当不能整除2，3，5则退出循环
+            # 如果这个数能被2整除 就继续整除
+            # 当不能被2整除 则用3除 以此类推
             while num % i == 0:
                 num = num / i
         # 如果剩余数字不是1 则这个数一定是其他质数 返回false
         return num == 1
+
 
