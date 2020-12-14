0c0e54aac6be658cb95361b618e5f28a3deaa5c0
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-12-14 15:34:30 -0800

    Update 0494_Target_Sum.py

--- 0494_Target_Sum.py
+++ 0494_Target_Sum.py
@@ -15,14 +15,45 @@ class Solution(object):
         length = 2 * nums_sum + 1
         dp = [[0] * length for _ in range(n)]
         # 注意初始化
+        # 如果nums[0]， 那么dp[0][0] = 2 而不是 1
         dp[0][nums_sum + nums[0]] = 1
         dp[0][nums_sum - nums[0]] = dp[0][nums_sum - nums[0]] + 1
+        
         for i in range(1, n):
             for j in range(length):
+                # 分别计算左右两边
                 l = dp[i - 1][j - nums[i]] if 0 <= j - nums[i] < length else 0
                 r = dp[i - 1][j + nums[i]] if 0 <= j + nums[i] < length else 0
                 dp[i][j] = l + r
-            print dp
+            # print dp
+        return dp[n-1][nums_sum+S]
+    
+    # 二维
+    def findTargetSumWays(self, nums, S):
+        """
+        :type nums: List[int]
+        :type S: int
+        :rtype: int
+        """
+        n = len(nums)
+        # 先判断如果全是正号或者全是负号的情况下 如果比s小 直接返回
+        nums_sum = sum(nums)
+        if abs(nums_sum) < abs(S): 
+            return 0
+        # 整个数组长度应该是算上全是负号那边
+        length = 2 * nums_sum + 1
+        dp = [[0] * length for _ in range(n)]
+        # 注意初始化
+        dp[0][nums_sum + nums[0]] = 1
+        dp[0][nums_sum - nums[0]] = dp[0][nums_sum - nums[0]] + 1  
+        for i in range(1, n):
+            for j in range(length):
+                # 分别计算左右两边
+                if 0 <= j - nums[i] < length:
+                    dp[i][j] += dp[i - 1][j - nums[i]] 
+                if 0 <= j + nums[i] < length:
+                    dp[i][j] += dp[i - 1][j + nums[i]] 
+            # print dp
         return dp[n-1][nums_sum+S]
     
     # 优化成一维
@@ -40,13 +71,16 @@ class Solution(object):
         dp = [0] * length
         dp[nums_sum + nums[0]] = 1
         dp[nums_sum - nums[0]] += 1
-        print dp
         for i in range(1, n):
+            # 因为dp[i]的取值可能需要左右两边的值相加 所以建立一个临时数组last 用来存储结果
+            last = [0] * length
             for j in range(length-1, -1, -1):
-                l = dp[j-nums[i]] if 0 <= j - nums[i] < length else 0
-                r = dp[j+nums[i]] if 0 <= j + nums[i] < length else 0
-                dp[j] = l + r
-            print dp
+                if 0 <= j - nums[i] < length:
+                    last[j] += dp[j-nums[i]] 
+                if 0 <= j + nums[i] < length:
+                    last[j] += dp[j+nums[i]]
+            dp = last
+            # print last, dp
         return dp[nums_sum+S]
     
 # 二维            
