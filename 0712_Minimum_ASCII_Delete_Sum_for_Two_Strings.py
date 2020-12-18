9295fcae4b92e604c6e66f73e0b53354241397eb
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-12-18 00:04:30 -0800

    Create 0712_Minimum_ASCII_Delete_Sum_for_Two_Strings.py

---
+++ 0712_Minimum_ASCII_Delete_Sum_for_Two_Strings.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def minimumDeleteSum(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: int
+        """
+        m = len(s1)
+        n = len(s2)
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        
+        # base case 初始化
+        for i in range(1, m+1):
+            dp[i][0] = dp[i-1][0] + ord(s1[i-1])
+        for j in range(1, n+1):
+            dp[0][j] = dp[0][j-1] + ord(s2[j-1])
+            
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                # if ord(s1[i-1]) == ord(s2[j-1]):
+                # 如果当前两个字符一样 那么删除操作应该等于上一层的值
+                if s1[i-1] == s2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                # 如果当前两个字符不一样 那么至少需要删除一个 看笔记吧
+                else:
+                    dp[i][j] = min(dp[i-1][j] + ord(s1[i-1]), dp[i][j-1] + ord(s2[j-1]))
+        # print dp
+        return dp[-1][-1]   
