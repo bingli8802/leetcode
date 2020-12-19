721787a112bbe258eb5b6fe565f22698b71722a8
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-12-18 22:06:55 -0800

    Update 0005_Longest_Palindromic_Substring.py

--- 0005_Longest_Palindromic_Substring.py
+++ 0005_Longest_Palindromic_Substring.py
@@ -6,15 +6,13 @@ class Solution(object):
         :rtype: str
         """
         if s == "":
-            return ""
-        
+            return ""  
         def isPalindrome(sub):
             m = len(sub)
             for i in range(m/2):
                 if sub[i] != sub[m-1-i]:
                     return False
-            return True
-                
+            return True             
         res = ""
         n = len(s)
         for i in range(n):
@@ -23,7 +21,25 @@ class Solution(object):
                     if j-i+1 > len(res): 
                         res = s[i:j]
         return res
-    # 优化  
+    
+    # 二维dp 不超时 但是比较慢
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """ 
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        res = ""
+        for i in range(n):
+            for j in range(i+1):
+                if s[i] == s[j] and (i - j < 2 or dp[i-1][j+1] == 1):
+                    dp[i][j] = 1
+                    if len(s[j:i+1]) > len(res):
+                        res = s[j:i+1]
+        return res
+    
+    # 优化 中心扩展法 不超时
     def longestPalindrome(self, s):
         """
         :type s: str
@@ -61,7 +77,7 @@ class Solution(object):
         for i in range(1, n):
             even = s[i-max_len:i+1]
             odd = s[i-max_len-1:i+1]
-            print even, odd
+            # print even, odd
             if i - max_len - 1 >= 0 and odd == odd[::-1]:
                 start = i - max_len - 1
                 max_len += 2
@@ -72,35 +88,8 @@ class Solution(object):
                 continue
         return s[start:start + max_len]
     
-    # 官方解法dp 
-    # 状态转移方程 p(i,j) = p(i+1, j-1) is true and s[i] == s[j]
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        n = len(s)
-        dp = [[False] * n for _ in range(n)]
-        ans = ""
-        # 枚举子串的长度 k+1
-        for k in range(n):
-            # 枚举子串的起点 i，可以通过 j=i+k 得到子串的结束位置
-            for i in range(n):
-                j = i + k
-                if j >= n:
-                    break
-                # 当长度是1 任何一个字母都是回文的
-                if k == 0:
-                    dp[i][j] = True
-                # 当长度是2 只要检查首位两个字母就行
-                elif k == 1:
-                    dp[i][j] = (s[i] == s[j])
-                # 当长度大于2 就要用到状态转移方程
-                else:
-                    dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
-                # 最后判断如果当前长度大于ans 更新结果
-                if dp[i][j] and k + 1 > len(ans):
-                    ans = s[i:j+1]
-        return ans
+
+    
+
 
 
