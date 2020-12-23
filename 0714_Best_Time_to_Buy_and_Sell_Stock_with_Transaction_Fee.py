675d3f85209728deaca880873822ffce9e15c2b6
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-12-22 22:13:16 -0800

    Update 0714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.py

--- 0714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.py
+++ 0714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.py
@@ -1,4 +1,22 @@
 class Solution(object):
+    # 东哥动态规划 二维
+    def maxProfit(self, prices, fee):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        n = len(prices)
+        dp = [[0, -prices[0]]] + [[0, 0] for _ in range(n - 1)]
+        # dp = [[0]*2 for _ in range(n)]
+        # dp[0][0] = 0
+        # dp[0][1] = -prices[0]
+        for i in range(1, n):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i] - fee)
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+        # print dp
+        return dp[-1][0]
+    
+    # 优化成常数级
     def maxProfit(self, prices, fee):
         """
         :type prices: List[int]
