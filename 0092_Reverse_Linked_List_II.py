edfb00c4f3f366fbc5a525ef2dcb7cb46b70bae6
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-12-28 16:57:15 -0800

    Update 0092_Reverse_Linked_List_II.py

--- 0092_Reverse_Linked_List_II.py
+++ 0092_Reverse_Linked_List_II.py
@@ -5,6 +5,7 @@
 #         self.next = next
 
 class Solution:
+    # 迭代
     def reverseBetween(self, head, m, n):
         """
         :type head: ListNode
@@ -44,3 +45,31 @@ class Solution:
         # con.next = prev
         tail.next = cur
         return head
+    
+    # 东哥递归
+    def reverseBetween(self, head, m, n):
+        """
+        :type head: ListNode
+        :type m: int
+        :type n: int
+        :rtype: ListNode
+        """
+        def reverseN(head,n):
+            if n == 1:
+                return head
+            # 以 head.next 为起点，需要反转前 n - 1 个节点
+            last = reverseN(head.next, n-1)
+            successor = head.next.next 
+            # 以head.next为开头的链表已经完成翻转，那么head.next.next正确指向后继节点
+            head.next.next = head
+            head.next = successor
+            return last
+        if m == 1:
+            return reverseN(head,n)
+        head.next = self.reverseBetween(head.next,m-1,n-1)
+        return head
+
+            
+        
+        
+        
