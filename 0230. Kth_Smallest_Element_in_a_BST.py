aad90bc787f5f165f62fc6b185251140820de553
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-12-29 15:29:52 -0800

    Update 0230. Kth_Smallest_Element_in_a_BST.py

--- 0230. Kth_Smallest_Element_in_a_BST.py
+++ 0230. Kth_Smallest_Element_in_a_BST.py
@@ -4,33 +4,48 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# 树不变
 class Solution(object):
+# 树经常改变怎么办？
+# 树不变
     def kthSmallest(self, root, k):
         """
         :type root: TreeNode
         :type k: int
         :rtype: int
         """
-        def dfs(node, k):
+        def dfs(node):
             if not node:
                 return None
             if node.left:
-                dfs(node.left, k)
+                dfs(node.left)
             res.append(node.val)
             if node.right:
-                dfs(node.right, k)
+                dfs(node.right)
         res = []
-        dfs(root, k)
+        dfs(root)
         if len(res) < k:
             return None
         else:
             return res[k - 1]
-# 树经常改变
-class Solution(object):
+    # 东哥解法   
     def kthSmallest(self, root, k):
         """
         :type root: TreeNode
         :type k: int
         :rtype: int
         """
+        self.rank = 0
+        self.res = 0
+        def dfs(node):
+            if not node:
+                return None
+            if node.left:
+                dfs(node.left)
+            self.rank += 1
+            if k == self.rank:
+                self.res = node.val
+                return
+            if node.right:
+                dfs(node.right)
+        dfs(root)
+        return self.res
