9fe1cdfd4c91a640bfe6d574cccbcbd73e2eccc2
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-12-30 15:29:37 -0800

    Update 0046_Permutations.py

--- 0046_Permutations.py
+++ 0046_Permutations.py
@@ -15,3 +15,14 @@ class Solution(object):
                 helper(li[:i] + li[i+1:], tmp_li + [li[i]], cnt + 1)
         helper(nums, [], 0)
         return res
+    
+    def permute(self, nums):
+        res = []
+        def backtrack(nums, tmp):
+            if not nums:
+                res.append(tmp)
+                return 
+            for i in range(len(nums)):
+                backtrack(nums[:i] + nums[i+1:], tmp + [nums[i]])
+        backtrack(nums, [])
+        return res
