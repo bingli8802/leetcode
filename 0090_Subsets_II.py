9e407c9d4fcd4f24247526ca0059dfef108d1db9
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2020-12-31 11:41:10 -0800

    Update 0090_Subsets_II.py

--- 0090_Subsets_II.py
+++ 0090_Subsets_II.py
@@ -34,3 +34,21 @@ class Solution(object):
                 helper(j+1, tmp+[nums[j]])
         helper(0, [])
         return res 
+    
+    # 参考东哥回溯算法框架模板
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        n = len(nums)
+        res = []
+        def backTrack(li, tmp):
+            res.append(tmp)
+            for i in range(len(li)):
+                if i >=1 and li[i] == li[i-1]:
+                    continue
+                backTrack(li[i+1:], tmp+[li[i]])
+        backTrack(nums, [])
+        return res
