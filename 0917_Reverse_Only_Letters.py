76537de56e8ebd7904287c3f12759300c362b299
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2021-01-20 14:11:02 -0800

    Update 0917_Reverse_Only_Letters.py

--- 0917_Reverse_Only_Letters.py
+++ 0917_Reverse_Only_Letters.py
@@ -4,6 +4,25 @@ class Solution(object):
         :type S: str
         :rtype: str
         """
+        i = 0
+        j = len(S) - 1
+        s = list(S)
+        while i < j:
+            if s[i].isalpha() and s[j].isalpha():
+                s[i], s[j] = s[j], s[i]
+                i += 1
+                j -= 1
+            else:
+                if not s[i].isalpha():
+                    i += 1
+                elif not s[j].isalpha():
+                    j -= 1
+        return "".join(s)
+    def reverseOnlyLetters(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
         new_S = []
         
         for i in S:
@@ -13,3 +32,4 @@ class Solution(object):
             if not S[c].isalpha():
                 new_S.insert(c,S[c])
         return "".join(new_S)
+            
