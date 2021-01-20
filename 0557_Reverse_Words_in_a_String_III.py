f08648ef4962da0a2cc640d6a79f5778322d6b52
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2021-01-20 14:12:14 -0800

    Update 0557_Reverse_Words_in_a_String_III.py

--- 0557_Reverse_Words_in_a_String_III.py
+++ 0557_Reverse_Words_in_a_String_III.py
@@ -1,5 +1,15 @@
-
 class Solution(object):
+    # 原地修改
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s_list = s.split(" ")
+        for i in range(len(s_list)):
+            s_list[i] = s_list[i][::-1]
+        return " ".join(s_list)
+    
     def reverseWords(self, s):
         """
         :type s: str
@@ -9,12 +19,7 @@ class Solution(object):
         li = s.split(" ")
         for i in li:
             """reverse each string in the list"""
-            """i = i[::-1]"""
-            i = "".join(reversed(i))
+            i = i[::-1]
+            # i = "".join(reversed(i)) # reversed特殊用法
             ret.append(i)
         return " ".join(ret)
-    
-        # for i in xrange(len(li)):
-        #     li[i] = li[i][::-1]
-        
-        # return ' '.join(li)
