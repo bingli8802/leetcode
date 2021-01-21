fc78c3b1e003ee9cb28fda66c9116fd6ab20dbba
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2021-01-20 17:57:25 -0800

    Update 0125_Valid_Palindrome.py

--- 0125_Valid_Palindrome.py
+++ 0125_Valid_Palindrome.py
@@ -7,7 +7,31 @@ class Solution(object):
         s = s.lower()
         li = [elem for elem in s if str(elem).isalnum()]
         
-        if li[:] == li[::-1]:
-            return True
-        else:
-            return False
+        # if li[:] == li[::-1]:
+        #     return True
+        # else:
+        #     return False
+        
+        return li[:] == li[::-1]
+    
+    # 双指针做法
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = s.lower()
+        i = 0
+        j = len(s)-1
+        while i < j:
+            if s[i].isalnum() and s[j].isalnum():
+                if s[i] == s[j]:
+                    i += 1
+                    j -= 1
+                else:
+                    return False
+            elif not s[i].isalnum():
+                i += 1
+            elif not s[j].isalnum():
+                j -= 1
+        return True
