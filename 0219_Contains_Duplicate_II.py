ccc04f2db2f943c1064cca50f16982ffd171063f
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2021-01-29 21:41:40 -0800

    Update 0219_Contains_Duplicate_II.py

--- 0219_Contains_Duplicate_II.py
+++ 0219_Contains_Duplicate_II.py
@@ -34,3 +34,16 @@ class Solution(object):
             # dict[nums[i]] = i
         return False
 
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        dic = {}
+        for i, v in enumerate(nums):
+            if v in dic and i - dic[v] <= k:
+                return True
+            elif v not in dic or i - dic[v] > k:
+                dic[v] = i
+        return False
