3482af724092d56917f06305db0ef49c0e09f181
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2021-01-30 17:44:43 -0800

    Update 0387_First_Unique_Character_in_a_String.py

--- 0387_First_Unique_Character_in_a_String.py
+++ 0387_First_Unique_Character_in_a_String.py
@@ -4,8 +4,27 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        for c in s:
-            if s.find(c) == s.rfind(c):
-                return s.find(c)
+        if not s:
+            return -1
+        res = float('inf')
+        dic = defaultdict(list)
+        for i, v in enumerate(s):
+            dic[v].append(i)
+        for val in dic.values():
+            if len(val) == 1:
+                res = min(res, val[0])
+        if res == float('inf'):
+            return -1
+        else:
+            return res
+        
+    # if index are the same     
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """    
+        for i in s:
+            if s.find(i) == s.rfind(i):
+                return s.find(i)
         return -1
-
