a2f84262812f8d412f8829f32618b1f4b5ca60f7
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2021-01-31 10:59:31 -0800

    Create 0378_Kth_Smallest_Element_in_a_Sorted_Matrix.py

---
+++ 0378_Kth_Smallest_Element_in_a_Sorted_Matrix.py
@@ -0,0 +1,64 @@
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        rec = sorted(sum(matrix, []))
+        return rec[k - 1]
+    
+    # minHeap 最小堆 
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        n = len(matrix)
+        pq = [(matrix[i][0], i, 0) for i in range(n)]
+        heapq.heapify(pq)
+
+        ret = 0
+        for i in range(k - 1):
+            print pq
+            num, x, y = heapq.heappop(pq)
+            if y != n - 1:
+                heapq.heappush(pq, (matrix[x][y + 1], x, y + 1))
+                print pq 
+                print '------'
+        return heapq.heappop(pq)[0]
+    
+    # 二分法 比较难理解
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        n = len(matrix)
+        if k == 1:
+            return matrix[0][0]
+        if k == n**n:
+            return matrix[-1][-1]
+
+        def check(mid):
+            i, j = n - 1, 0
+            num = 0
+            while i >= 0 and j < n:
+                if matrix[i][j] <= mid:
+                    num += i + 1
+                    j += 1
+                else:
+                    i -= 1
+            return num >= k
+
+        left, right = matrix[0][0], matrix[-1][-1]
+        
+        while left < right:
+            mid = (left + right) // 2
+            if check(mid):
+                right = mid
+            else:
+                left = mid + 1 
+        return left
