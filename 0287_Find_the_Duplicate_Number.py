a40c019c4b750b177b853464b5b64d76a1e627a3
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2021-01-31 15:32:11 -0800

    Update 0287_Find_the_Duplicate_Number.py

--- 0287_Find_the_Duplicate_Number.py
+++ 0287_Find_the_Duplicate_Number.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    # 二分法
     def findDuplicate(self, nums):
         """
         :type nums: List[int]
@@ -9,15 +10,26 @@ class Solution(object):
         while left < right:
             cnt = 0
             mid = (left+right)/2
-#           计算小于mid的数字
+            # 计算小于mid的数字
             for i in nums:
                 if i <= mid:
                     cnt += 1
-#           如果小于mid数字的个数<=它自己 那么重复的数字一定在[mid+1，right]之间
+            # mid = 3, cnt = 2, 如果小于3个数 小于3: 那么重复的数字一定在4，5之间，left右移 缩小范围
             if cnt <= mid:
                 left = mid + 1
-#           反之 如果小于mid数字的个数>他自己 那么重复的数字一定在[left,mid]之间
+            # mid = 3, cnt = 4, 如果小于3个数 大于3: 那么重复的数字一定在1，3之间 right左移 缩小范围
             else:
                 right = mid
         return right
-                
+    
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dic = dict(collections.Counter(nums))
+        for i, v in dic.items():
+            if v > 1:
+                return i
+        
+        
