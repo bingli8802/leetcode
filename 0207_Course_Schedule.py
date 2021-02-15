ed5fcdcaa50b9b06c424bd777fe105daf0f008f5
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2021-02-14 16:47:21 -0800

    Create 0207_Course_Schedule.py

---
+++ 0207_Course_Schedule.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        indegree = [0 for _ in range(numCourses)]
+        adjancy = [[] for _ in range(numCourses)]
+        
+        for cur, pre in prerequisites:
+            indegree[cur] += 1
+            adjancy[pre].append(cur)
+        
+        q = deque()
+        
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+        cnt = 0
+        while q:
+            course = q.popleft()
+            cnt += 1
+            for adj in adjancy[course]:
+                indegree[adj] -= 1
+                if indegree[adj] == 0:
+                    q.append(adj)
+        return cnt == numCourses
